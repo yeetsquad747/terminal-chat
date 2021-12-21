6911bad0540fd2944168ea9e6d8a5bccc58ead2c
yeetsquad747 <74121237+yeetsquad747@users.noreply.github.com>
2021-12-21 12:29:23 -0800

    Adding terminal-chat stuff

---
+++ app/index.js
@@ -0,0 +1,58 @@
+import chalk from "chalk";
+import readline from "readline";
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+import io from "socket.io-client";
+console.clear();
+
+let server_url = "";
+rl.question(
+  chalk.whiteBright(
+    "What is the url of the server(example: http://192.168.1.108:3000)? "
+  ),
+  (answer) => {
+    server_url = answer;
+    const socket = io(server_url);
+
+    let id = "";
+    let buffer = "";
+
+    function chat() {
+      rl.question(chalk.magenta("» "), (answer) => {
+        buffer = `${chalk.cyan(id)} : ${chalk.green(answer)}`;
+        if (!answer) {
+          rl.question(chalk.redBright("No sending blank messages!"), () => {
+            chat();
+          });
+        } else {
+          socket.emit("message", buffer);
+        }
+        chat();
+      });
+    }
+
+    socket.on("connect", () => {
+      rl.pause();
+      rl.question(
+        chalk.whiteBright("😃 What's your name?\n "),
+        (answer) => {
+          socket.emit(
+            "message",
+            `👤  ${chalk.green(answer)} has joined the chat`
+          );
+          id = answer;
+          chat();
+        }
+      );
+      socket.on("msg", function (data) {
+        if (buffer != data) {
+          console.log("\n" + data);
+          chat();
+        }
+      });
+    });
+  }
+);
---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import chalk from "chalk";
+let app = express();
+
+let http = require("http").Server(app);
+let io = require("socket.io")(http);
+
+io.on("connection", function (socket) {
+  io.emit("msg", chalk.greenBright("New connection to server received"));
+  socket.on("message", function (data) {
+    io.emit("msg", data);
+    console.log(data);
+  });
+});
+
+http.listen(3000, function () {
+  console.log("listening on 3000");
+});
